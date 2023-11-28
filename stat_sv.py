faebeeedf107e7f41851d5583b0cb4518ac24034
Qi Bao <108071362+netwon123@users.noreply.github.com>
2023-11-28 17:20:50 +0800

    Update stat_sv.py

--- stat_sv.py
+++ stat_sv.py
@@ -1,29 +1,36 @@
+import argparse
 
-# 定义需要分类的结构变异类型
-categories = ['INS', 'DEL', 'DUP', 'INV', 'BND']
-
-# 打开VCF文件
-with open('TT_pop.vcf', 'r') as f:
-    # 初始化一个字典来存储结构变异类型和数量
+def count_sv_types(vcf_file):
+    categories = ['INS', 'DEL', 'DUP', 'INV', 'BND']
     sv_types = {'unknown': 0}
-    # 遍历文件中的每一行
-    for line in f:
-        # 检查行是否以#开头，如果是，则跳过该行
-        if line.startswith('#'):
-            continue
-        # 将行分割成一个列表
-        line_list = line.strip().split('\t')
-        # 获取结构变异类型，需要根据vcf修改第一个split的值
-        sv_type = line_list[7].split(';')[1].split('=')[1]
-        # 将不在分类列表中的结构变异类型归类为unknown
-        if sv_type not in categories:
-            sv_type = 'unknown'
-        # 增加该结构变异类型的数量
-        if sv_type in sv_types:
-            sv_types[sv_type] += 1
-        else:
-            sv_types[sv_type] = 1
 
-# 打印每个结构变异类型及其数量
-for sv_type, count in sv_types.items():
-    print('{}: {}'.format(sv_type, count))
+    with open(vcf_file, 'r') as f:
+        for line in f:
+            if line.startswith('#'):
+                continue
+            line_list = line.strip().split('\t')
+            sv_type = line_list[7].split(';')[1].split('=')[1]  # 根据vcf文件的具体格式进行修改
+            if sv_type not in categories:
+                sv_type = 'unknown'
+            if sv_type in sv_types:
+                sv_types[sv_type] += 1
+            else:
+                sv_types[sv_type] = 1
+
+    return sv_types
+
+def main():
+    parser = argparse.ArgumentParser(description='Count structural variant types in a VCF file')
+    parser.add_argument('vcf_file', help='Input VCF file')
+    args = parser.parse_args()
+    
+    result = count_sv_types(args.vcf_file)
+    
+    # 输出结果
+    output = args.vcf_file + "_output.txt"
+    with open(output, 'w') as f:
+        f.write("File Name\tunknown\tINV\tDEL\tINS\tDUP\tBND\n")
+        f.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(args.vcf_file, result['unknown'], result['INV'], result['DEL'], result['INS'], result['DUP'], result['BND']))
+
+if __name__ == "__main__":
+    main()
